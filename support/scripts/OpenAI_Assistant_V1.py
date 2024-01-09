f2876afa9d9b576865046ded6b17061ef5af2f75
nrehder@allgeier.ch <nrehder@allgeier.ch>
2024-01-09 18:49:22 +0100

    2024010901

--- support/scripts/OpenAI_Assistant_V1.py
+++ support/scripts/OpenAI_Assistant_V1.py
@@ -24,8 +24,7 @@
 #————————————————————
 
 # Download Python packages (run the below command in terminal if packages have not yet been installed)
-#pip install -r requirements.txt
-#pip install -r /support/scripts/requirements/requirements.txt
+#pip install -r C:\Python\openai-lab\support\requirements\requirements.txt
 
 # Import required libraries
 import os
